a29d4ed1c1ed7cbd07f3d0d1280e2968ae2d8c96
b1rsuk <b1rsu_k@protonmail.ch>
2022-05-28 23:40:08 +0300

    end

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
-var cors = require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-const menu = ['Прямые диваны', 'Угловые диваны', 'Угловые диваны', 'Распродажа диванов'];
+const menu = [  { '': [{title: 'Прямые диваны', marginAndDiscount: [0, 1], id: 0}], 'Прямые диваны': [{title: 'Модульные диваны', marginAndDiscount: [0, 0], id: 1}, {title:'Каркасные диваны', marginAndDiscount: [0, 0], id: 2}], 'Модульные диваны': [{title: 'Норм диваны', marginAndDiscount: [0, 1], id: 3}, {title: 'Натуральные диваны', marginAndDiscount: [0, 1], id: 4}], 'Норм диваны':[], 'Натуральные диваны': [], 'Каркасные диваны': [] }]; // '': [] обязательно
 const users = [
     {
         id: 0, // id
@@ -29,7 +29,7 @@ const users = [
             note: 'aa',
             password: '324324',
             fullName: 'POP OPS Sd',
-            birchDay: '223.34.34',
+            birchDay: 's223.34.34',
         }
     },
 ];
@@ -63,14 +63,32 @@ const product = [
         name: 'Png',
         price: [
             3450,
+            5000
+        ],
+        about: 'abount',
+        detalis: 'detalis',
+        category: [],
+        label: 'sd',
+        demensions: [],
+        scheme: ['/product/scheme/scheme.png', '/product/scheme/scheme.png'],
+        material: [],
+        id: 0
+    }, 
+    {
+        src: ['/product/png.png', '/product/divan.png'],
+        name: 'Png2',
+        price: [
+            3470,
             0
         ],
         about: 'abount',
         detalis: 'detalis',
-        category: 'category',
+        category: [],
         label: 'sd',
         demensions: [],
-        id: 0
+        scheme: [],
+        material: [{name: 'name', list: [{name: '123'}]}],
+        id: 1
     }, 
    
 ];
@@ -91,6 +109,10 @@ app.post('/authorization', (req, res) => {
     if ( req.body.user == 'borsuk' && req.body.password == 'borsuk') return res.send(true);
     res.send(false);
 });
+
+app.get('/table/category/', (req, res) => {
+    res.send(menu);
+})
 
 app.post('/edit/product', (req, res) => { // редактирвание продукта
     const { name, price, about, detalis, category, src, label, id } = req.body;
@@ -144,8 +166,8 @@ app.get('/limit/services', (req, res) => {
 });
 
 app.post('/create/product', (req, res) => {
-    const { name, price, about, detalis, category, src, label, id } = req.body;
-    console.log({ name, price, about, detalis, category, src, label, id });
+    const { name, price, about, detalis, category, src, label, demensions, material, id } = req.body;
+    console.log({ name, price, about, detalis, category, src, label, demensions, material, id });
     res.end();
 });
 
@@ -161,7 +183,14 @@ app.post('/delete/image/', (req, res) => {
 
 app.post('/save/image', (req, res) => {
     const { img, name } = req.body;
+    console.log(img, name)
     res.send('/product/qr.png');
+});
+
+app.post('/save/scheme', (req, res) => {
+    const { img, name } = req.body;
+    console.log(img, name)
+    res.send('/product/scheme/scheme.png');
 });
 
 app.post('/save/user', (req, res) => {
@@ -190,10 +219,46 @@ app.post('/edit/category', (req, res) => {
 });
 
 app.post('/create/category', (req, res) => {
-    const { name, margin, discount } = req.body;
-    console.log({ name, margin, discount });
+    const { menu, category } = req.body;
+    console.log(menu, category);
     res.end();
 });
+
+app.post('/demensions', (req, res) => {
+    const { value } = req.body;
+    console.log(value);
+    res.end();
+});
+
+app.post('/material', (req, res) => {
+    const { value } = req.body;
+    console.log(value);
+    res.end();
+});
+
+app.get('/delete/demension/:index', (req, res) => {
+    const { index } = req.params;
+    console.log(index)
+    res.end();
+});
+
+app.post('/delete/category', (req, res) => {
+    const { id, category } = req.body;
+    console.log(id, category)
+    res.end();
+})
+
+app.get('/delete/material/:index', (req, res) => {
+    const { index } = req.params;
+    console.log(index);
+    res.end();
+})
+
+app.post('/delete/scheme', (req, res) => {
+    const { src } = req.body;
+    console.log(src);
+    res.end();
+})
 
 app.listen(5000, function () {
     console.log('CORS-enabled web server listening on port 80')
